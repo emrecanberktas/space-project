6f1174cd312e137e16b3e6cf013e2ecb71e235f1
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2022-10-24 15:07:34 +0300

    project complete

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -7,31 +7,33 @@ function Technology(props) {
   return (
     <div className="technology">
       <Navbar />
-      <div className="tecnology__leftside">
-        <div className="technology__leftside__title">
-          <h2>03</h2>
-          &emsp;
-          <h3>SPACE LAUNCH 101</h3>
-        </div>
-        <div className="technology__buttons">
-          <Link to="launchVehicle" className="technology__button">
-            1
-          </Link>
-          <Link to="spacePort" className="technology__button">
-            2
-          </Link>
-          <Link to="spaceCapsule" className="technology__button">
-            3
-          </Link>
+      <div className="technology__container">
+        <div className="tecnology__leftside">
+          <div className="technology__leftside__title">
+            <h2>03</h2>
+            &emsp;
+            <h3>SPACE LAUNCH 101</h3>
+          </div>
+          <div className="technology__buttons">
+            <Link to="/launchVehicle" className="technology__button">
+              1
+            </Link>
+            <Link to="/spacePort" className="technology__button">
+              2
+            </Link>
+            <Link to="/spaceCapsule" className="technology__button">
+              3
+            </Link>
+          </div>
         </div>
         <div className="technology__leftside__container">
           <p className="terminology">THE TERMINOLOGY...</p>
           <p className="technology__title">{props.title}</p>
           <p className="technology__content">{props.content}</p>
         </div>
-      </div>
-      <div className="technology__rightside">
-        <img src={props.image} />
+        <div className="technology__rightside">
+          <img src={props.image} />
+        </div>
       </div>
     </div>
   );
